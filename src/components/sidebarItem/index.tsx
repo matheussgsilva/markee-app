d682feb394a25b370142088ac03cdf4ec9380270
Matheussgsilva <matheussgsilva@hotmail.com>
2022-04-24 19:14:10 -0300

    Add props with type

--- src/components/sidebarItem/index.tsx
+++ src/components/sidebarItem/index.tsx
@@ -1,9 +1,18 @@
 import * as C from './style'
 
-export const SidebarItem = () => {
+import sheet from 'assets/sheet.svg'
+
+type Props =
+ {
+   name : string,
+ }
+export const SidebarItem = ({ name }: Props) => {
   return (
     <C.Container>
-      Item
+      <li>
+        <a href='/'><img src={sheet} alt='folha de papel' />{name}</a>
+        <button>x</button>
+      </li>
     </C.Container>
   )
 }
--- src/components/sidebarItem/style.ts
+++ src/components/sidebarItem/style.ts
@@ -1,5 +1,48 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 15px;
+    padding: 0px 30px;
 
+    li {
+        display: flex;
+        align-items: center;
+        list-style: none;
+        justify-content: space-between;
+        width: 100%;
+        padding: 5px 10px;
+
+        &:hover {
+            background-color: ${({ theme }) => theme.colors.lightBlack};
+            border-radius: 5px;
+        }
+
+        & a {
+            display: flex;
+            align-items: center;
+            text-decoration: none;
+            font-size: 1.5rem;
+            color: ${({ theme }) => theme.colors.white};
+        }
+
+        & a img {
+            margin-right: 1rem;
+            color: ${({ theme }) => theme.colors.primary};
+        }
+
+        & button {
+            opacity: 0;
+            background-color: transparent;
+            border: none;
+            font-size: 1.5rem;
+            color: ${({ theme }) => theme.colors.white};
+        }
+
+        &:hover button {
+            opacity: 1;
+        }
+    }
 `

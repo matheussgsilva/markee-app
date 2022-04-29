9bbfe4112bd7329277a52998bea61a5009ac0a0d
Matheussgsilva <matheussgsilva@hotmail.com>
2022-04-29 19:56:03 -0300

    Add imports, pass prop to Container and create conditionals to render button and StatusIcon

--- src/components/sidebarItem/index.tsx
+++ src/components/sidebarItem/index.tsx
@@ -1,15 +1,18 @@
 import * as C from './style'
 
-import sheet from 'assets/sheet.svg'
+import sheet from 'ui/assets/sheet.svg'
+import sheetActive from 'ui/assets/file-text.png'
+import { StatusIcon } from 'ui/assets/statusIcon'
 import { MarkeeItem } from 'resources/files/type'
 
 export const SidebarItem = ({ name, active, status, id }: MarkeeItem) => {
   console.log(active, status)
   return (
-    <C.Container>
+    <C.Container active={active}>
       <li>
-        <a href={`/data/${id}`}><img src={sheet} alt='folha de papel' />{name}</a>
-        <button>x</button>
+        <a href={`/data/${id}`}><img src={`${!active ? sheet : sheetActive}`} alt='folha de papel' />{name}</a>
+        {!active && <button>x</button>}
+        {active && <StatusIcon status={status} />}
       </li>
     </C.Container>
   )

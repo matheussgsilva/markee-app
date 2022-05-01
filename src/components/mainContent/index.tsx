309ebc69615ee52e8b2fc5189bf8156d87d94081
Matheussgsilva <matheussgsilva@hotmail.com>
2022-05-01 16:08:19 -0300

    Config the highlight

--- src/components/mainContent/index.tsx
+++ src/components/mainContent/index.tsx
@@ -1,9 +1,23 @@
 import { useState, ChangeEvent } from 'react'
 import { marked } from 'marked'
+import 'highlight.js/styles/github.css'
 
 import * as C from './style'
 
 import fileSheet from 'ui/assets/file-text.png'
+
+import('highlight.js').then(hljs => {
+  const hl = hljs.default
+
+  marked.setOptions({
+    highlight: (code, language) => {
+      if (language && hl.getLanguage(language)) {
+        return hl.highlight(code, { language }).value
+      }
+      return hl.highlightAuto(code).value
+    },
+  })
+})
 
 export const MainContent = () => {
   const [content, setContent] = useState('')

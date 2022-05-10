2f98d69e4243ed8e1c09880a76011b235748e55d
Matheussgsilva <matheussgsilva@hotmail.com>
2022-05-10 19:19:12 -0300

    Import BrowserRouter

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,8 @@ import { MainContent } from 'components/mainContent'
 import { Sidebar } from 'components/sidebar'
 import { useFile } from 'resources/files/use-file'
 
+import { BrowserRouter } from 'react-router-dom'
+
 import * as C from './style'
 
 export function App () {
@@ -16,19 +18,21 @@ export function App () {
   } = useFile()
 
   return (
-    <C.Container>
-      <Sidebar
-        files={files}
-        onNewFile={handleCreateNewFile}
-        onSelectFile={handleSelectFile}
-        onRemoveFile={handleRemoveFile}
-      />
-      <MainContent
-        inputRef={inputRef}
-        file={files.find(file => file.active === true)}
-        onUpdateFileName={handleUpdateFileName}
-        onUpdateFileContent={handleUpdateFileContent}
-      />
-    </C.Container>
+    <BrowserRouter>
+      <C.Container>
+        <Sidebar
+          files={files}
+          onNewFile={handleCreateNewFile}
+          onSelectFile={handleSelectFile}
+          onRemoveFile={handleRemoveFile}
+        />
+        <MainContent
+          inputRef={inputRef}
+          file={files.find(file => file.active === true)}
+          onUpdateFileName={handleUpdateFileName}
+          onUpdateFileContent={handleUpdateFileContent}
+        />
+      </C.Container>
+    </BrowserRouter>
   )
 }

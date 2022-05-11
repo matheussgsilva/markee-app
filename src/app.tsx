7686a7116d7f05433d261c751d533c1df81de4d5
Matheussgsilva <matheussgsilva@hotmail.com>
2022-05-11 19:48:11 -0300

    Delete BrowserRouter

--- src/app.tsx
+++ src/app.tsx
@@ -2,8 +2,6 @@ import { MainContent } from 'components/mainContent'
 import { Sidebar } from 'components/sidebar'
 import { useFile } from 'resources/files/use-file'
 
-import { BrowserRouter } from 'react-router-dom'
-
 import * as C from './style'
 
 export function App () {
@@ -18,21 +16,19 @@ export function App () {
   } = useFile()
 
   return (
-    <BrowserRouter>
-      <C.Container>
-        <Sidebar
-          files={files}
-          onNewFile={handleCreateNewFile}
-          onSelectFile={handleSelectFile}
-          onRemoveFile={handleRemoveFile}
-        />
-        <MainContent
-          inputRef={inputRef}
-          file={files.find(file => file.active === true)}
-          onUpdateFileName={handleUpdateFileName}
-          onUpdateFileContent={handleUpdateFileContent}
-        />
-      </C.Container>
-    </BrowserRouter>
+    <C.Container>
+      <Sidebar
+        files={files}
+        onNewFile={handleCreateNewFile}
+        onSelectFile={handleSelectFile}
+        onRemoveFile={handleRemoveFile}
+      />
+      <MainContent
+        inputRef={inputRef}
+        file={files.find(file => file.active === true)}
+        onUpdateFileName={handleUpdateFileName}
+        onUpdateFileContent={handleUpdateFileContent}
+      />
+    </C.Container>
   )
 }

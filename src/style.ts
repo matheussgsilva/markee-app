10d3094d3d781efeda2ccd6b3fdd50beff991347
Matheussgsilva <matheussgsilva@hotmail.com>
2022-05-18 19:53:12 -0300

    Add justify-content property to App style

--- src/style.ts
+++ src/style.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components/macro'
 
 export const Container = styled.main`
     display: flex;
+    justify-content: flex-start;
     width: 100vw;
     overflow-x: hidden;
 `

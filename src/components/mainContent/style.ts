31d4e669e6c1e03b8534f5a36fc071d8c634bfd3
Matheussgsilva <matheussgsilva@hotmail.com>
2022-05-17 22:20:18 -0300

    Change max-width to flex-grow in Content

--- src/components/mainContent/style.ts
+++ src/components/mainContent/style.ts
@@ -21,7 +21,7 @@ export const ContentLeft = styled.div`
     }
 
     @media (max-width: 500px) {
-        max-width: 500px;
+        flex-grow: 1;
     }
 `
 
@@ -60,7 +60,7 @@ export const ContentRight = styled.div`
     padding: 0px 30px;
 
     @media (max-width: 500px) {
-        max-width: 500px;
+        flex-grow: 1;
         border-left: none;
         border-top: 2px solid rgba(30, 41, 59, 0.12);
     }

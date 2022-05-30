8e086e17b09cc696be9376aec60dda6927d0b49e
Matheus Silva <81831589+Matheussgsilva@users.noreply.github.com>
2022-05-30 15:10:46 -0300

    Change App display from flex to grid

--- src/style.ts
+++ src/style.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components/macro'
 
 export const Container = styled.main`
-    display: flex;
-    justify-content: flex-start;
+    display: grid;
+    grid-template-columns: minmax(150px, 20%) 1fr;
     width: 100vw;
     overflow-x: hidden;
 `

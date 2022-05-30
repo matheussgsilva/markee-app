244a3d2c66d67d3161df124a606247f31b78c0d6
Matheus Silva <81831589+Matheussgsilva@users.noreply.github.com>
2022-05-30 15:40:57 -0300

    Change the grid values

--- src/style.ts
+++ src/style.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components/macro'
 
 export const Container = styled.main`
     display: grid;
-    grid-template-columns: minmax(300px, 20%) 1fr;
+    grid-template-columns: 20vw 1fr;
     width: 100vw;
     overflow-x: hidden;
 `
